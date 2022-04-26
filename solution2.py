20097f2c138068b4ff055094972ac5a49f954d07
HarakaHeartbroken <christophervfoster@gmail.com>
2022-04-25 20:02:45 -0700

    Solution 2 added

--- solution2.py
+++ solution2.py
@@ -1,1 +1,14 @@
 # Write code for algorithm 2 below
+#2. Write a function that prints the natural numbers from 1 to n.
+
+def countUp(num):
+    count = 0
+    #Base Case
+    if count = num :
+        print count
+        return
+    #Recursive Case
+    else:
+        count = count + 1
+        countUp(num-count)
+        print count

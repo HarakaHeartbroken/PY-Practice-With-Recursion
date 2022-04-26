a0452404a1e09f67bee31c187a18b93275217b5d
HarakaHeartbroken <christophervfoster@gmail.com>
2022-04-25 19:54:31 -0700

    Solution 1 Added

--- solution1.py
+++ solution1.py
@@ -1,1 +1,13 @@
 # Write code for algorithm 1 below
+# Write a program that takes a 
+# positive number as an argument and prints the numbers from n to zero.
+
+
+def program(num):
+    #Base Case
+    if num == 0:
+        return
+    #Recursive Case
+    else:
+        print(num)
+        program(num-1)
